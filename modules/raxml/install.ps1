8e1cb48b4ce0c64e80f377d8d76555208448f244
Johannes Hengstler <mail.jhengstler@gmail.com>
2025-04-08 18:23:01 +0200

    remove windows-style newlines from bash-scripts before executing them

--- modules/raxml/install.ps1
+++ modules/raxml/install.ps1
@@ -15,6 +15,9 @@ git checkout 5c07bb65fd50b7d739b4e9cebfbae0475b44a51d
 git submodule update --init --recursive
 
 Write-Host "Compiling RAxML under WSL"
+ # Make sure the compile script is well-formed
+wsl dos2unix ../compile.sh
+
 # Build the project under WSL
 wsl ../compile.sh
 Pop-Location
