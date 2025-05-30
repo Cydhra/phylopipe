e8e5fd9b9bc3637de8751fa2668fcfb21111594a
Johannes Hengstler <mail.jhengstler@gmail.com>
2025-05-30 17:07:54 +0200

    switch to a dev version of treestats because of a bug in MPD

--- modules/treestats/packages.R
+++ modules/treestats/packages.R
@@ -1,1 +1,4 @@
-install.packages("treestats", repos = "https://cran.uni-muenster.de/")
+install.packages("remotes", repos = "https://cran.uni-muenster.de/")
+
+# install.packages("treestats", repos = "https://cran.uni-muenster.de/")
+remotes::install_github("thijsjanzen/treestats@cb9432211c76c66f73f8d71be0a893839f8d7e0c")
