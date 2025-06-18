bc6d01dc63ea90b9c37c6a415cd8de721eb8920d
Johannes Hengstler <mail.jhengstler@gmail.com>
2025-06-18 13:08:40 +0200

    updated commit of treestats module, so we get some sanity checks resolved

--- modules/treestats/packages.R
+++ modules/treestats/packages.R
@@ -1,4 +1,4 @@
 install.packages("remotes", repos = "https://cran.uni-muenster.de/")
 
 # install.packages("treestats", repos = "https://cran.uni-muenster.de/")
-remotes::install_github("thijsjanzen/treestats@cb9432211c76c66f73f8d71be0a893839f8d7e0c")
+remotes::install_github("thijsjanzen/treestats@bad88938d148aa7d884ea75a271ac873b8a34781")
