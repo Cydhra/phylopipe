adbe803b731aeecde3b2eeabc2b2b10fd12e07ad
Johannes Hengstler <mail.jhengstler@gmail.com>
2025-06-24 16:17:36 +0200

    adapt raxml install script for linux

--- modules/raxml/install.ps1
+++ modules/raxml/install.ps1
@@ -3,6 +3,8 @@ Push-Location $PSScriptRoot
 # load config
 . ..\..\config.ps1
 
+Import-Module $PSScriptRoot/modules/linux
+
 $INSTALL_FILE = "raxml-ng-2"
 
 if (Test-Path $INSTALL_FILE) {
@@ -19,18 +21,18 @@ git submodule update --init --recursive
 
 Write-Host "Compiling RAxML under WSL"
  # Make sure the compile script is well-formed
-wsl dos2unix ../compile.sh
+Invoke-OnLinux -Path dos2unix ../compile.sh
 
 # Build the project under WSL
-wsl ../compile.sh
+Invoke-OnLinux -Path ../compile.sh
 Pop-Location
 
 # Copy the compiled binary to the scripts directory
-cp raxml-ng/bin/raxml-ng-2 $INSTALL_FILE
+Copy-Item raxml-ng/bin/raxml-ng-2 $INSTALL_FILE
 
 Write-Host "Cleaning up..."
 # Clean up
-rm -Recurse -Force raxml-ng
+Remove-Item -Recurse -Force raxml-ng
 
 Pop-Location
 Write-Host "Done."
