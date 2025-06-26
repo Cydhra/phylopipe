4d07790b76ba46d50afe88aaac980cbf04451a84
Johannes Hengstler <mail.jhengstler@gmail.com>
2025-06-26 17:05:52 +0200

    update raxml version to include fix of autoconfig

--- config.ps1
+++ config.ps1
@@ -2,6 +2,6 @@ $CONSEL_URL = "https://github.com/shimo-lab/consel"
 
 $RAXML_URL = "https://github.com/amkozlov/raxml-ng"
 $RAXML_BRANCH = "dev"
-$RAXML_COMMIT = "5c07bb65fd50b7d739b4e9cebfbae0475b44a51d"
+$RAXML_COMMIT = "c056d0e6a429fafd71cfec9e190e2a640f250d85"
 
 $NEWICK_URL = "https://github.com/rcedgar/newick/releases/download/v1.0.1429/newick_v1.0.1429_linux"
