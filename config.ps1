b4a82edaf304704940b6ac2c9b4cd2183e2cba51
Johannes Hengstler <mail.jhengstler@gmail.com>
2025-07-17 16:30:32 +0200

    update raxml to latest commit

--- config.ps1
+++ config.ps1
@@ -2,7 +2,7 @@ $CONSEL_URL = "https://github.com/shimo-lab/consel"
 
 $RAXML_URL = "https://github.com/amkozlov/raxml-ng"
 $RAXML_BRANCH = "dev"
-$RAXML_COMMIT = "c056d0e6a429fafd71cfec9e190e2a640f250d85"
+$RAXML_COMMIT = "11dc3c49efdc0f8462180688f4fad3a78fc71114"
 
 $NEWICK_URL = "https://github.com/rcedgar/newick/releases/download/v1.0.1429/newick_v1.0.1429_linux"
 
