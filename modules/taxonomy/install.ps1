29d161aba42c3ab5de7d5d90db21dfb5c6cbf508
Johannes Hengstler <mail.jhengstler@gmail.com>
2026-01-28 17:58:59 +0100

    installation script for taxonomy database

---
+++ modules/taxonomy/install.ps1
@@ -0,0 +1,18 @@
+Push-Location $PSScriptRoot
+
+# make sure mongodb is installed
+#& ../mongodb/install.ps1
+
+# load config
+. ..\..\config.ps1
+
+Import-Module $PSScriptRoot/../mongodb
+
+#& ./download.ps1
+
+Start-Mongo
+Import-MongoConfiguration "aggregate_hierarchy.js"
+& ./import.ps1
+
+Pop-Location
+Write-Host "Done."
