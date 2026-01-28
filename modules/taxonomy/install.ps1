eb3a04abe9272519bfd68c31715449cf494d4e85
Johannes Hengstler <mail.jhengstler@gmail.com>
2026-01-28 18:33:11 +0100

    re-enable commented lines in taxonomy installer

--- modules/taxonomy/install.ps1
+++ modules/taxonomy/install.ps1
@@ -1,14 +1,14 @@
 Push-Location $PSScriptRoot
 
 # make sure mongodb is installed
-#& ../mongodb/install.ps1
+& ../mongodb/install.ps1
 
 # load config
 . ..\..\config.ps1
 
 Import-Module $PSScriptRoot/../mongodb
 
-#& ./download.ps1
+& ./download.ps1
 
 Start-Mongo
 Import-MongoConfiguration "aggregate_hierarchy.js"
