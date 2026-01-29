3db88f4cd740f7e592df26f2f0f8af762f72281d
Johannes Hengstler <mail.jhengstler@gmail.com>
2026-01-29 11:32:56 +0100

    switch from github back to cran package for treestats, as the fixes are released and this avoids local compilation

--- modules/treestats/packages.R
+++ modules/treestats/packages.R
@@ -1,4 +1,2 @@
 install.packages("remotes", repos = "https://cran.uni-muenster.de/")
-
-# install.packages("treestats", repos = "https://cran.uni-muenster.de/")
-remotes::install_github("thijsjanzen/treestats@bad88938d148aa7d884ea75a271ac873b8a34781")
+install.packages("treestats", repos = "https://cran.uni-muenster.de/")
