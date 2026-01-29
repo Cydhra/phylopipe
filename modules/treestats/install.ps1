9199ec8ff77db060132c9902258fea1bdc352b63
Johannes Hengstler <mail.jhengstler@gmail.com>
2026-01-29 11:25:55 +0100

    treestats installer uses rscript module instead of local install

--- modules/treestats/install.ps1
+++ modules/treestats/install.ps1
@@ -1,26 +1,12 @@
 Push-Location $PSScriptRoot
 
-$r_install = "R"
-$r_path = [System.IO.Path]::Combine($r_install, "R-4.5.0-win.exe")
-$r_url = "https://cran.r-project.org/bin/windows/base/R-4.5.0-win.exe"
+# Ensure rscript is installed
+& $PSScriptRoot/../rscript/install.ps1
 
-if (-not (Test-Path ([System.IO.Path]::Combine($r_install, "bin\Rscript.exe")))) {
-    if (-not (Test-Path $r_install)) {
-        Write-Host "Downloading R..."
-        New-Item -Path $r_install -ItemType Directory > $null
+Import-Module $PSScriptRoot/../rscript
 
-        curl $($r_url) -o $r_path
-    } else {
-        Write-Host "R is already downloaded."
-    }
+Invoke-R packages.R
 
-    Write-Host "Installing R..."
-    Start-Process -FilePath $r_path -ArgumentList "/VERYSILENT /DIR=`"$r_install`" /v/qn /NORESTART /NOCANCEL /SUPPRESSMSGBOXES /CURRENTUSER /SP- /LANG=en /MERGETASKS=!desktopicon" -Wait
-} else {
-    Write-Host "R is already installed."
-}
-
-$rscript_path = [System.IO.Path]::Combine($r_install, "bin\Rscript.exe")
-& $rscript_path packages.R
+Write-Host "Successfully installed treestats."
 
 Pop-Location
