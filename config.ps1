f8e7dea137d77e15d7c6672a3e522c787dd0983d
Johannes Hengstler <mail.jhengstler@gmail.com>
2026-02-24 18:11:55 +0100

    update raxml module

--- config.ps1
+++ config.ps1
@@ -2,7 +2,7 @@ $CONSEL_URL = "https://github.com/shimo-lab/consel"
 
 $RAXML_URL = "https://github.com/amkozlov/raxml-ng"
 $RAXML_BRANCH = "dev"
-$RAXML_COMMIT = "8d8f4440c57a3754839d58458af6eed178d9fa40"
+$RAXML_COMMIT = "b8f1e59391f7bea1c338560d338d39fe61b875c2"
 
 $NEWICK_URL = "https://github.com/rcedgar/newick"
 $NEWICK_COMMIT = "83701cb4040ac5d364eb1403c7732af4f900d6bf"
