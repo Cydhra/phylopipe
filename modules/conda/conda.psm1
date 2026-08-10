5a73b9a8f91c4d63f124f5fd85d4647a2daa48ed
Johannes Hengstler <mail.jhengstler@gmail.com>
2026-08-10 17:14:22 +0200

    change conda module to just activate conda when imported

--- modules/conda/conda.psm1
+++ modules/conda/conda.psm1
@@ -1,44 +1,35 @@
 Import-Module $PSScriptRoot/../linux
 
-function Get-CondaHook {
-     Return "$PSScriptRoot/miniconda3/shell/condabin/conda-hook.ps1"
-}
+$CondaHook = "$PSScriptRoot/miniconda3/shell/condabin/conda-hook.ps1"
 
-<#
- .SYNOPSIS
- Get the name of the conda activate which is activated with conda activate when the hook has been loaded.
-#>
-function Set-CondaEnvironment {
-    $CondaHook = Get-CondaHook
+# check if hook is present, in which case we prefer the local conda instance
+if (Test-Path $CondaHook) {
     . $CondaHook
-    conda activate phylopipe
+} else {
+    conda --version > $null
+
+    if ($LASTEXITCODE) {
+        $Prompt = Read-Host -Prompt "No conda available. Do you want to install a local instance? (Y/N)"
+
+        if ($Prompt -ieq "y") {
+            . "$PSScriptRoot/install.ps1"
+            . $CondaHook
+        } else {
+            Return 1
+        }
+    }
 }
 
-<#
- .SYNOPSIS
- Execute a provided command line in the conda environment.
-
- .DESCRIPTION
- Runs the provided command in the installed conda environment. This does not have to be a conda command, and
- consequently "conda" still needs to be part of the command if it is a conda command.
-
- .PARAMETER Path
- Linux-compatible path to the executable.
-
- .PARAMETER CommandLine
- The full command line provided to the executable. Beware that powershell may interpret some arguments differently
- than linux shells, so they have to be escaped to be properly passed by the linux shell.
-#>
-function Invoke-InConda {
-    param(
-        [Parameter(Mandatory = $true, Position=0)]
-        [string[]] $Command,
-
-        [Parameter(Mandatory = $false, ValueFromRemainingArguments)]
-        [string[]] $Args
-    )
-
-    Set-CondaEnvironment
-
-    & $Command @Args
+$Env
+conda info | ForEach-Object {
+    if ($_ -match "active environment : (?<env>.*)") {
+        $Env = $Matches["env"]
+    }
 }
+
+conda list -n phylopipe > $null
+if ($LASTEXITCODE) {
+    conda create -n phylopipe
+}
+
+conda activate phylopipe
