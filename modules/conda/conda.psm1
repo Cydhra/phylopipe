4aaef676521d068f79842552ac8e3ac9f8e60f58
Johannes Hengstler <mail.jhengstler@gmail.com>
2026-08-11 11:46:47 +0200

    add a function to conda to test whether a package is installed

--- modules/conda/conda.psm1
+++ modules/conda/conda.psm1
@@ -32,12 +32,29 @@ if ($LASTEXITCODE) {
     conda create -n phylopipe
 }
 
+<#
+ .SYNOPSIS
+ Ensure the phylopipe channel is active.
+#>
 function Enter-Conda {
     conda activate phylopipe
 }
 
 Enter-Conda
 
+<#
+ .SYNOPSIS
+ Install a conda package from a local path into the phylopipe environment.
+
+ .PARAMETER Name
+ Name of the conda package
+
+ .PARAMETER Path
+ File path to the channel where the package is located.
+
+ .RETURN
+ A boolean value indicating whether the package was installed successfully.
+#>
 function Install-LocalCondaPackage {
     Param (
         [Parameter(Mandatory)]
@@ -56,8 +73,23 @@ function Install-LocalCondaPackage {
 
     # has to be linux-style path
     conda install -y -c "../../channel" $Name --override-channels
+
+    Return $LASTEXITCODE -eq 0
 }
 
+<#
+ .SYNOPSIS
+ Install a conda package from a remote channel into the phylopipe environment.
+
+ .PARAMETER Name
+ Name of the conda package
+
+ .PARAMETER Channel
+ Optionally the channel where to get the package.
+
+ .RETURN
+ A boolean value indicating whether the package was installed successfully.
+#>
 function Install-CondaPackage {
     Param (
         [Parameter(Mandatory = $false)]
@@ -73,4 +105,25 @@ function Install-CondaPackage {
         conda install -y -n phylopipe $Name
     }
 
+    Return $LASTEXITCODE -eq 0
+}
+
+<#
+ .SYNOPSIS
+ Check if a conda package is installed in phylopipe.
+
+ .PARAMETER Name
+ Name of the conda package
+
+ .RETURN
+ A boolean value indicating whether the package is installed.
+#>
+function Test-CondaPackage {
+    Param (
+        [Parameter(Mandatory)]
+        [string] $Name
+    )
+
+    conda list -n phylopipe $Name > $null
+    Return $LASTEXITCODE -eq 0
 }
