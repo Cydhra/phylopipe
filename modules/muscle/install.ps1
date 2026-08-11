23e40ed868f81f6062d1956c9f243454a59e6e2b
Johannes Hengstler <mail.jhengstler@gmail.com>
2026-08-11 16:32:15 +0200

    modernized muscle installer

--- modules/muscle/install.ps1
+++ modules/muscle/install.ps1
@@ -9,27 +9,25 @@ New-Item -ItemType Directory -Path $INSTALL_DIR -ErrorAction SilentlyContinue > 
 
 if (Test-Path "$INSTALL_DIR/muscle") {
     Pop-Location
-    Write-Host "muscle already installed."
+    Write-Host -ForegroundColor Yellow "muscle already installed."
     exit
 }
 
-Write-Host "Cloning muscle at commit $MUSCLE_COMMIT"
+Write-Host -ForegroundColor Yellow "Cloning muscle at commit $MUSCLE_COMMIT"
 git clone $MUSCLE_URL muscle
 Push-Location muscle
 
 git checkout $MUSCLE_COMMIT
 Push-Location src
-
-Write-Host "Running build script..."
+Write-Host -ForegroundColor Yellow "Running build script..."
 ConvertTo-UnixLineEnding -Path ./build_linux.bash
 Invoke-OnLinux -Path /usr/bin/env bash ./build_linux.bash
 Pop-Location
 
 Copy-Item ./bin/muscle ../$INSTALL_DIR/muscle
-
-Write-Host "Cleaning up..."
+Write-Host -ForegroundColor Yellow "Cleaning up..."
 Pop-Location
 Remove-Item -Recurse -Force muscle
 
 Pop-Location
-Write-Host "Successfully installed muscle."
+Write-Host -ForegroundColor Yellow "Successfully installed muscle."
